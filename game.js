3ef6242599d84fc5f2b986e39062553f756e901f
Duc <145208899+DucDevelop@users.noreply.github.com>
2024-09-11 12:40:06 +0200

    Create function to play one round

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 const CHOICES = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     // picks random number bewteen 0 - 2 (inclusive) 
@@ -14,4 +16,43 @@ function getHumanChoice() {
         return choice;
     }
     return getComputerChoice();
+}
+
+function playRound (humanChoice, computerChoice) {
+    let computerWin = false;
+    let userWin = false;
+
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        userWin = true;
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerWin = true;
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        userWin = true;
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        computerWin = true;
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        userWin = true;
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        computerWin = true;
+    }
+
+
+
+    if (computerWin) {
+        console.log(`You lose! "${computerChoice}" beats "${humanChoice}"`);
+        computerScore++;
+    }
+    else if (userWin) {
+        console.log(`You win! "${humanChoice}" beats "${computerChoice}"`);
+        humanScore++;
+    }
+    else {
+        console.log(`Tie! Both chose "${humanChoice}"`);
+    }
+    
 }
