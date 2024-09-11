42462ddcc0bf56472e4e56ef4f013d6187d44391
Duc <145208899+DucDevelop@users.noreply.github.com>
2024-09-11 12:17:17 +0200

    Create function to get random choice

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+function getComputerChoice() {
+    const CHOICES = ["rock", "paper", "scissors"];
+    // picks random number bewteen 0 - 2 (inclusive) 
+    let randomChoice = Math.floor(Math.random() * 3);
+    return CHOICES[randomChoice];
+}
