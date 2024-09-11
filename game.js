3d779c7f8f27edae97f82a678922705b76dc8cc3
Duc <145208899+DucDevelop@users.noreply.github.com>
2024-09-11 12:23:49 +0200

    Create function to get user choice

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
+const CHOICES = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    const CHOICES = ["rock", "paper", "scissors"];
     // picks random number bewteen 0 - 2 (inclusive) 
     let randomChoice = Math.floor(Math.random() * 3);
     return CHOICES[randomChoice];
+}
+
+function getHumanChoice() {
+    
+    // picks random number bewteen 0 - 2 (inclusive) 
+    let choice = prompt('Please enter one of the following: "rock", "paper", "scissors"').toLocaleLowerCase().trim()
+    if (CHOICES.includes(choice)) {
+        return choice;
+    }
+    return getComputerChoice();
 }
